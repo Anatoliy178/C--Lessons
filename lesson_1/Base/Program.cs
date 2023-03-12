b76c94fc2bf3e4c3defb21491d8fd9312dfe4951
Anatoliy178 <lukyanov001@live.ru>
2023-03-12 11:53:55 +0300

    Выбор большего числа

---
+++ lesson_1/Base/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 2;
+int b = 12;
+int c = 8;
+int d = 1;
+int e = 3;
+
+int max = a;
+
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.WriteLine(max);
