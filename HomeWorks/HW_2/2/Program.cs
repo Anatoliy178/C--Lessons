25042ba7755d3dfd5e5d307a9c996f2d8759e52f
Anatoliy178 <lukyanov001@live.ru>
2023-03-12 21:55:55 +0300

    2 задача

---
+++ HomeWorks/HW_2/2/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или
+ сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6                    */
+
+// int a;
+// Console.WriteLine("Введите число: ");
+// int.TryParse(Console.ReadLine()!, out a);
+// int a1 = a % 100 ;
+// Console.WriteLine(a1);
+
+
+int number;
+Console.WriteLine("Введите число: ");
+int.TryParse(Console.ReadLine()!, out number);
+
+int thirdNumber(int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+bool check(int number)
+{
+    if (number < 100)
+    {
+        return false;
+    }
+    else
+        return true;
+}
+
+if (check(number) != true)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine($"Третья цифра числа {number} является {thirdNumber(number)}");
+}
