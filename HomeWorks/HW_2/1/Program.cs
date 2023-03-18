3debf6839a00cbfaebf86bf7d3df61ee139516eb
Anatoliy178 <lukyanov001@live.ru>
2023-03-18 17:15:00 +0300

    первая загрузка в GitHub

--- HomeWorks/HW_2/1/Program.cs
+++ HomeWorks/HW_2/1/Program.cs
@@ -1,15 +1,39 @@
 ﻿#pragma warning disable
-// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе
 // показывает вторую цифру этого числа.
 
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
 
-int a;
-Console.WriteLine("Введите число");
-int.TryParse(Console.ReadLine()!);
 
-if()
+int Promt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int number = Promt("Введите трёхзначное число: ");
+if (number < 100 || number > 1000)
+{
+    Console.WriteLine("Вы ввели не трёхзначное число. Пробуйте ёщё раз!");
+    return;
+}
+Console.WriteLine($"Введённое число: '{number}'");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Вторая цифра: '{secondRank}'");
 
 
+
+
+// int a;
+// Console.WriteLine("Введите число");
+// int.TryParse(Console.ReadLine()!);
+
+// if (a >= 100)
+// {
+//     a /= 10;
+//     Console.WriteLine(a);
+// }
---
+++ lesson_3/1/Program.cs
@@ -0,0 +1,80 @@
+﻿// Вид 1
+void Metod1()
+{
+    Console.WriteLine("Автор ...");
+}
+
+// Metod1();
+
+
+// Вид 2
+void Metod2(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+// Metod2(msg: "Текст сообщения");
+
+void Metod21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+
+// Metod21(msg: "Текст 1", count: 4);
+// Metod21(count: 5, msg: "Хай");
+
+
+// Вид 3
+
+int Metod3()
+{
+    return DateTime.Now.Year;
+}
+
+// int year = Metod3();
+// Console.WriteLine(year);
+
+
+// Вид 4
+
+// string Metod4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+string Metod4(int count, string text)
+{
+    string result = String.Empty;
+
+    for (int i = 0; i < 10; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+// string res = Metod4(10, "absd;");
+// Console.WriteLine(res);
+
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
