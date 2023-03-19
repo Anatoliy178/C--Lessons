c9a56f2bbd6a75bda601fdcbe9af14aaa1432a5d
Anatoliy178 <lukyanov001@live.ru>
2023-03-19 21:24:26 +0300

    решил вторую задачу 3 ДЗ

--- HomeWorks/HW_3/3/Program.cs
+++ HomeWorks/HW_3/3/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+string number;
+
+// int a;
+Console.WriteLine($"Введите число для проверки: ");
+number = Console.ReadLine()!;
+
+Console.WriteLine($"{number[0]} + {number[1]}");
+// int.TryParse(Console.ReadLine()!, out a);
+
+// if (a <= 10000 || a <= 99999)
+// {
+//     int result = a % 100;
+//     Console.WriteLine(result);
+// }
+
+// string firstFriend = "Maria";
+// string secondFriend = "Sage";
+// Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+// Console.WriteLine($"{firstFriend} {firstFriend.Length}");
+// Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
