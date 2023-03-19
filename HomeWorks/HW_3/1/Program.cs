f18f810783b4f8390994b031cc09f03a1cdce713
Anatoliy178 <lukyanov001@live.ru>
2023-03-19 10:02:02 +0300

    Ветвь семинаров

--- HomeWorks/HW_3/1/Program.cs
+++ HomeWorks/HW_3/1/Program.cs
@@ -3,3 +3,23 @@
 
 // 3-> 1, 8, 27
 //5-> 1, 8, 27, 64, 125
+
+Console.WriteLine($"Введите число для вычисления: ");
+int number = int.Parse(Console.ReadLine()!);
+
+functionCube(number);
+
+// void PrintCube(int[] array)
+// {
+//   Console.Write(int[] array);
+// }
+
+int functionCube(int number)
+{
+    int[] array;
+    for (int i = 1; i <= number; i++)
+    {
+        array[i] = i * i * i;
+    }
+    return array[number];
+}
