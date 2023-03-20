51fcda9332ad038038d19577e5d5af1087a64456
Anatoliy178 <lukyanov001@live.ru>
2023-03-20 20:47:51 +0300

    3 домашнее задание

---
+++ HomeWorks/HW_3/4/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = 454;
+int Ab = a;
+
+while (a > 0)
+{
+    int rev = 0;
+    int d = a % 10;
+    rev = rev * 10 + d;
+    a = a / 10;
+    if (rev == Ab)
+    {
+        Console.WriteLine($"Это число полиндром!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine($"Это число не полиндром.");
+    }
+}
+Console.WriteLine("Hello, World!");
