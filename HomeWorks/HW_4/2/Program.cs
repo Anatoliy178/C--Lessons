8aeda929669ef4821b70bc0e23f2a6f99a3cc044
Anatoliy178 <lukyanov001@live.ru>
2023-03-22 20:50:42 +0300

    решение 2й  задачи 4 ДЗ

--- HomeWorks/HW_4/2/Program.cs
+++ HomeWorks/HW_4/2/Program.cs
@@ -3,3 +3,31 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+using System;
+using System.Globalization;
+
+
+int number = Promt("Введите число: ");
+Console.WriteLine($"Сумма всех чисел в {number} = {SumNumbers(number)}.");
+
+
+int Promt(string message)
+{
+  Console.Write(message);
+  int result = int.Parse(Console.ReadLine()!);
+  return result;
+}
+
+int SumNumbers (int number)
+{
+  int result = 0;
+  while(number > 0)
+  {
+    result += number % 10;
+    number = number / 10;
+  }
+  return result;
+}
+
+
