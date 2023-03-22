275a2bf69ef77487cdeee1549968c62fe8bd11f4
Anatoliy178 <lukyanov001@live.ru>
2023-03-22 19:44:29 +0300

    Добавил папку с 4 ДЗ и удалил лишнее

---
+++ HomeWorks/HW_4/1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+using System;
+
+int Promt(string message)
+{
+  Console.Write(message);
+  string inputRead = Console.ReadLine()!;
+  int result = int.Parse(inputRead);
+  return result;
+}
+
+int PowerBase(int powerBase, int exponent)
+{
+  int power = 1;
+  for (int i = 0; i < exponent; i++)
+  {
+    power *= powerBase;    
+  }
+  return power;
+}
+
+bool Validator(int exponent)
+{
+  if (exponent < 0)
+  {
+    Console.WriteLine($"степень не может быть меньше 0!");
+    return false;
+  }
+  return true;
+}
+
+
+int powerBase = Promt($"Введте число: ");
+int exponent = Promt($"Введите степени: ");
+if(Validator(exponent))
+{
+  Console.WriteLine($"Число {powerBase}  в степение {exponent}  равно {PowerBase(powerBase, exponent)}");
+}
